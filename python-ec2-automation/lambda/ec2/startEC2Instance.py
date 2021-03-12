16aed3ffebb2a27042d93c287f742da015acfad1
Michael D Clayton <michael.d.clayton@hotmail.com>
2021-03-12 01:47:20 -0500

    init

---
+++ python-ec2-automation/lambda/ec2/startEC2Instance.py
@@ -0,0 +1,17 @@
+import boto3
+
+#run these programs as lambda functions in AWS
+
+#sample input
+#{
+#  "instance-ids": "x-xxxxxxxx,x-xxxxxxxx"
+#}
+
+def lambda_handler(event, context):
+    client = boto3.client('ec2')
+    my_list = event['instance-ids'].split(",")
+    response = client.start_instances(
+        InstanceIds=my_list
+        )
+    print('Started Instance: '+event['instance-ids'])
+    return response['StartingInstances']
---
+++ python-ec2-automation/lambda/ec2/stopEC2Iinstance.py
@@ -0,0 +1,16 @@
+import boto3
+
+#run these programs as lambda functions in AWS
+
+#sample input
+#{
+#  "instance-ids": "x-xxxxxxxx,x-xxxxxxxx"
+#}
+
+def lambda_handler(event, context):
+    my_list = event['instance-ids'].split(",")
+    client = boto3.client('ec2')
+    response = client.stop_instances(InstanceIds=my_list)
+    return response;
+    
+    
